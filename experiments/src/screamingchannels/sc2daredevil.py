9a4b1bacb5455e6587150bb764e376bd024a2b99
muchlikeacat <112901878+muchlikeacat@users.noreply.github.com>
2022-09-15 12:35:41 -0400

    Update sc2daredevil.py

--- experiments/src/screamingchannels/sc2daredevil.py
+++ experiments/src/screamingchannels/sc2daredevil.py
@@ -2,7 +2,7 @@ from os import chdir, getcwd
 from os.path import isfile, abspath
 
 # from load import load_traces, load_all
-from load import generic_load
+from .load import generic_load
 
 def main(trace_directory, trace_name, number):
     trace_directory = abspath(trace_directory)
@@ -34,21 +34,21 @@ def main(trace_directory, trace_name, number):
     print("trace_type=f")
     print("transpose=true")
     print("index=0")
-    print("nsamples=%d"% traces.shape[1])
-    print("trace=tracefile %d %d"%(traces.shape[0], traces.shape[1]))
+    print(("nsamples=%d"% traces.shape[1]))
+    print(("trace=tracefile %d %d"%(traces.shape[0], traces.shape[1])))
     print("")
     print("[Guesses]")
     print("files=1")
     print("guess_type=u")
     print("transpose=true")
-    print("guess=plaintext %d %d"%(textin.shape[0], textin.shape[1]))
+    print(("guess=plaintext %d %d"%(textin.shape[0], textin.shape[1])))
 
 
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("Usage: python3 %s TRACEDIR NAMEPATTERN [NUMBER]" % __file__)
+        print(("Usage: python3 %s TRACEDIR NAMEPATTERN [NUMBER]" % __file__))
         exit(-1)
     stop_trace = int(sys.argv[3]) if len(sys.argv) > 3 else 0
 
