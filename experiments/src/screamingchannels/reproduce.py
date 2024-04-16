e0f6725701cd7e5cc0df7e6cf115cf1aa3733999
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-04-16 18:40:17 +0200

    Duplicate the collect command for new command

--- experiments/src/screamingchannels/reproduce.py
+++ experiments/src/screamingchannels/reproduce.py
@@ -572,6 +572,259 @@ def collect(config, target_path, name, average_out, plot, plot_out, max_power, r
         radio.quit()
 
 @cli.command()
+@click.argument("config", type=click.File())
+@click.argument("target-path", type=click.Path(exists=True, file_okay=False))
+@click.option("--name", default="",
+              help="Identifier for the experiment (obsolete; only for compatibility).")
+@click.option("--average-out", type=click.Path(dir_okay=False),
+              help="File to write the average to (i.e. the template candidate).")
+@click.option("--plot/--no-plot", default=False, show_default=True,
+              help="Plot the results of trace collection.")
+@click.option("--plot-out", type=click.Path(dir_okay=False),
+              help="File to write the plot to (instead of showing it dynamically).")
+@click.option("--max-power/--no-max-power", default=False, show_default=True,
+              help="Set the output power of the device to its maximum.")
+@click.option("--raw/--no-raw", default=False, show_default=True,
+              help="Save the raw IQ data.")
+@click.option("--saveplot/--no-saveplot", default=True, show_default=True,
+              help="Save the plot of the results of trace collection.")
+@click.option("-p", "--set-power", default=0, show_default=True,
+              help="If set, sets the device to a specific power level (overrides --max-power)")
+def collect(config, target_path, name, average_out, plot, plot_out, max_power, raw, saveplot, set_power):
+    """
+    Collect traces for an attack.
+
+    The config is a JSON file containing parameters for trace analysis; see the
+    definitions of FirmwareConfig and CollectionConfig for descriptions of each
+    parameter.
+    """
+    # NO-OP defaults for mode dependent config options for backwards compatibility
+    cfg_dict = json.load(config)
+    cfg_dict["firmware"].setdefault('conventional', False)
+    cfg_dict["firmware"].setdefault('mask_mode', 0)
+    cfg_dict["firmware"].setdefault('slow_mode_sleep_time', 0.001)
+    cfg_dict["firmware"].setdefault('fixed_vs_fixed', False)
+    cfg_dict["firmware"].setdefault('fixed_plaintext', False)
+    cfg_dict["collection"].setdefault('traces_per_point_multiplier', 1.2)
+    cfg_dict["collection"].setdefault('hackrf_gain', 0)
+    cfg_dict["collection"].setdefault('hackrf_gain_bb', 44)
+    cfg_dict["collection"].setdefault('hackrf_gain_if', 40)
+    cfg_dict["collection"].setdefault('plutosdr_gain', 64)
+    cfg_dict["collection"].setdefault('usrp_gain', 40)
+    cfg_dict["collection"].setdefault('keep_all', False)
+    cfg_dict["collection"].setdefault('channel', 0)
+
+    collection_config = CollectionConfig(**cfg_dict["collection"])
+    firmware_config = FirmwareConfig(**cfg_dict["firmware"])
+
+    if firmware_config.mode == "tinyaes":
+        firmware_mode = TINY_AES_MODE
+    elif firmware_config.mode == "tinyaes_slow":
+        firmware_mode = TINY_AES_MODE_SLOW
+    elif firmware_config.mode == "maskaes":
+        firmware_mode = MASK_AES_MODE
+    elif firmware_config.mode == "maskaes_slow":
+        firmware_mode = MASK_AES_MODE_SLOW
+    elif firmware_config.mode == "hwcrypto":
+        firmware_mode = HW_CRYPTO_MODE
+    elif firmware_config.mode == "hwcrypto_keygen":
+        firmware_mode = HW_CRYPTO_KEYGEN_MODE
+    elif firmware_config.mode == "hwcrypto_ecb":
+        firmware_mode = HW_CRYPTO_ECB_MODE
+    elif firmware_config.mode == "hwcrypto_slow":
+        firmware_mode = HW_CRYPTO_MODE_SLOW
+    elif firmware_config.mode == "power":
+        firmware_mode = POWER_ANALYSIS_MODE
+    else:
+        raise Exception("Unsupported mode %s; this is a bug!" % firmware_config.mode)
+
+    # assert (not plot) or (collection_config.num_traces_per_point <= 500), \
+        # "Plotting a lot of data might lock up the computer! Consider reducing " \
+        # "num_traces_per_point in the configuration file or enforce limits on resource consumption..."
+
+    # Signal post-processing will drop some traces when their quality is
+    # insufficient, so let's collect more traces than requested to make sure
+    # that we have enough in the end.
+    num_traces_per_point = int(collection_config.num_traces_per_point * collection_config.traces_per_point_multiplier)
+
+    # number of points
+    num_points = int(collection_config.num_points)
+
+    # fixed vs fixed
+    fixed_vs_fixed = firmware_config.fixed_vs_fixed
+
+    # Generate the plaintexts
+    if fixed_vs_fixed:
+        plaintexts = ['\x00'*16 for _trace in range(num_points)]
+    else:
+        plaintexts = [os.urandom(16)
+                    for _trace in range(1 if firmware_config.fixed_plaintext else num_points)]
+    
+    with open(path.join(target_path, 'pt_%s.txt' % name), 'w') as f:
+        f.write('\n'.join(p.hex() for p in plaintexts))
+
+    # Generate the key(s)
+    if firmware_mode.have_keys:
+        if fixed_vs_fixed:
+            keys = ['\x00'*16 if i%2==0 else '\x30'*16 for i in range(num_points)]
+        else:
+            keys = [os.urandom(16)
+                    for _key in range(1 if firmware_config.fixed_key else num_points)]
+        with open(path.join(target_path, 'key_%s.txt' % name), 'w') as f:
+            f.write('\n'.join(k.hex() for k in keys))
+
+    # If requested, reset target
+    if YKUSH_PORT != 0:
+        l.debug('Resetting device using ykush port %d' % YKUSH_PORT)
+        system("ykushcmd -d %d" % YKUSH_PORT)
+        system("ykushcmd -u %d" % YKUSH_PORT)
+        time.sleep(3)
+
+
+
+    with _open_serial_port() as ser:
+        if YKUSH_PORT != 0:
+            print((ser.readline()))
+
+        # tmp increase power
+        #l.debug('POWERPOWER')
+        #ser.write(b'p')
+        #print ser.readline()
+        #ser.write(b'0')
+        #print ser.readline()
+        if set_power != 0:
+            l.debug('Setting power level to '+str(set_power))
+            ser.write(('p'+str(set_power)).encode('UTF-8'))
+            ser.readline()
+            ser.readline()
+        elif max_power:
+            l.debug('Setting power to the  maximum')
+            ser.write(b'p0')
+            ser.readline()
+            ser.readline()
+
+        if firmware_config.conventional:
+            l.debug('Starting conventional mode, the radio is off')
+        else:
+            l.debug('Selecting channel')
+            ser.write(b'a')
+            print((ser.readline()))
+            ser.write(b'%02d\n'%collection_config.channel)
+            print((ser.readline()))
+            if firmware_config.modulate:
+                l.debug('Starting modulated wave')
+                ser.write(b'o')     # start modulated wave
+                print((ser.readline()))
+            else:
+                l.debug('Starting continuous wave')
+                ser.write(b'c')     # start continuous wave
+
+        l.debug('Entering test mode')
+        ser.write(firmware_mode.mode_command.encode()) # enter test mode
+        print((ser.readline()))
+
+        if firmware_mode.repetition_command:
+            l.debug('Setting trace repitions')
+            ser.write(('n%d\r\n' % num_traces_per_point).encode())
+            print((ser.readline()))
+
+        if firmware_mode.have_keys and firmware_config.fixed_key:
+            # The key never changes, so we can just set it once and for all.
+            _send_key(ser, keys[0])
+
+        if firmware_config.fixed_plaintext:
+            # The plaintext never changes, so we can just set it once and for all.
+            _send_plaintext(ser, plaintexts[0])
+
+        if firmware_config.mode == 'maskaes' or firmware_config.mode == 'maskaes_slow':
+            l.debug('Setting masking mode to %d', firmware_config.mask_mode)
+            ser.write(('%d\r\n' % firmware_config.mask_mode).encode())
+            print((ser.readline()))
+
+        # Initialize the radio client.
+        radio = soapysdr.MySoapySDRsClient()
+            
+        # with click.progressbar(plaintexts) as bar:
+            # for index, plaintext in enumerate(bar):
+        index = 0
+        with click.progressbar(list(range(num_points)), label="Collecting") as bar:
+            # for index, plaintext in enumerate(bar):
+            while index < num_points:
+                if firmware_mode.have_keys and not firmware_config.fixed_key:
+                    _send_key(ser, keys[index])
+
+                if not firmware_config.fixed_plaintext:
+                    if firmware_config.mode == "hwcrypto_keygen":
+                        _send_init(ser, plaintexts[index])
+                    else:
+                        _send_plaintext(ser, plaintexts[index])
+
+                print("Start instrumentation #{}...".format(index))
+
+                # Start non-blocking recording for a pre-configured duration.
+                radio.record_start()
+                time.sleep(0.03)
+
+                if RADIO == Radio.USRP_B210_MIMO or RADIO == Radio.USRP_B210:
+                    time.sleep(0.08)
+                    # time.sleep(0.04)
+
+                if firmware_mode.repetition_command:
+                    # The test mode supports repeated actions.
+                    l.debug('Start repetitions')
+                    ser.write(firmware_mode.action_command.encode())
+                    ser.readline() # wait until done
+                else:
+                    for _iteration in range(num_traces_per_point):
+                        time.sleep(firmware_config.slow_mode_sleep_time)
+                        ser.write(firmware_mode.action_command.encode()) # single action
+
+                time.sleep(0.09)
+                try:
+                    # Wait the end of the recording.
+                    radio.record_stop()
+                    # Save on-disk and reinit the radio for future recording.
+                    radio.accept()
+                    radio.save()
+                except Exception as e:
+                    print("ERROR: From radio client: {}".format(e))
+                    print("INFO: Restart current recording...")
+                    continue
+
+                try:
+                    trace_amp, trace_phr, trace_i, trace_q, trace_i_augmented, trace_q_augmented = analyze.extract(OUTFILE, collection_config, average_out, plot, target_path, saveplot, index, return_zero=False)
+                except Exception as e:
+                    print("ERROR: From extraction function: {}".format(e))
+                    print("INFO: Restart current recording...")
+                    continue
+
+                np.save(os.path.join(target_path,"amp_%s_%d.npy"%(name,index)),trace_amp)
+                np.save(os.path.join(target_path,"phr_%s_%d.npy"%(name,index)),trace_phr)
+                np.save(os.path.join(target_path,"i_%s_%d.npy"%(name,index)),trace_i)
+                np.save(os.path.join(target_path,"q_%s_%d.npy"%(name,index)),trace_q)
+                if index < 30:
+                    plt.plot(trace_amp); figure = plt.gcf(); figure.set_size_inches(32, 18)
+                    plt.savefig(os.path.join(target_path,"amp_%s_%d.png"%(name,index))); plt.clf()
+                # np.save(os.path.join(target_path,"i_augmented_%s_%d.npy"%(name,index)),trace_i_augmented)
+                # np.save(os.path.join(target_path,"q_augmented_%s_%d.npy"%(name,index)),trace_q_augmented)
+                if raw:
+                    save_raw(OUTFILE, target_path, index, name)
+
+                # Update index and click progress bar.
+                index += 1
+                bar.update(1)
+
+        ser.write(b'q')     # quit tiny_aes mode
+        print((ser.readline()))
+        ser.write(b'e')     # turn off continuous wave
+
+        time.sleep(1)
+        ser.close()
+
+        # Quit the server.
+        radio.quit()
+
+@cli.command()
 @click.option("--plot/--no-plot", default=False, show_default=True,
               help="Whether to plot the data after recording.")
 def cw_with_regswitch(plot):
